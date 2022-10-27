ef21cc891122f238a57936869183b6acba60c403
sankshit <sankshit.pandoh@matelabs.in>
2022-10-27 11:40:18 +0530

    Added more test miners

--- test.js
+++ test.js
@@ -32,6 +32,30 @@ setTimeout(() => {
             }
         )
     )
+
+    echoCoin.addNewBlock (
+        new cryptoBlock (
+            3 ,
+            Date.now(),
+            {
+                sender: "Rick Sanchez",
+                rececipent: "Morty Sanchez",
+                quantity: 143
+            }
+        )
+    )
+
+    echoCoin.addNewBlock (
+        new cryptoBlock (
+            4 ,
+            Date.now(),
+            {
+                sender: "Morty Sanchez",
+                rececipent: "Poopy Butthole",
+                quantity: 1111
+            }
+        )
+    )
     
     console.log(JSON.stringify(echoCoin, null, 4));
 }, 500)
